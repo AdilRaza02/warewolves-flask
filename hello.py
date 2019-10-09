44388921e7d0ec8e119c44f5fbbae7a661c94338
unknown <adilraza2@hotmail.com>
2019-10-09 18:36:43 +0200

    add login route -test

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Developed by: Warewolves'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    print(username)
+    print(password)
 
 if __name__ == '__main__':
     app.run()
